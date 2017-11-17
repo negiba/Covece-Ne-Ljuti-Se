a393593b00cb9017ad5f4f97a4daadf84f403a19
negiba <negiba@gmail.com>
2017-11-17 12:36:24 +0100

    Boki uradila f-ju za random kockicu

--- gameplay.js
+++ gameplay.js
@@ -9,22 +9,46 @@ var players = [],
     firstTime = true,
     turn = false; // nije jos iskoriscena
 
-// Funkcija za bacanje kockice, nema opciju vrtenja random brojeva vec samo izbacuje random broj
-// Ima opciju za ponovni klik ukoliko se dobije 6
+// Boki uradila funkciju za bacanje kockice, sada ima opciju vrtenja random kockice
+// Sad treba srediti za ponovni klik ukoliko se dobije 6
+
 function kocka() {
     document.getElementById("middle").addEventListener("click", function () {
-        var randomNumber = cube[Math.floor(Math.random() * 6)];
-        console.log(randomNumber);
+        var randomNumber,
+        counter = 0;
         if (cubeClicked == true) {
-            if (randomNumber == 6) {
-                document.getElementById("middle").innerHTML = randomNumber;
-                cubeClicked = true;
-            }
-            else {
-                document.getElementById("middle").innerHTML = randomNumber;
-                cubeClicked = false;
-            }
+            var cubeClick = setInterval(function () {
+                var randomNumber = cube[Math.floor(Math.random() * 6)];
+                var images = "/slike/" + randomNumber + ".png";
+                document.getElementById("middle").style.background = "url(" + images + ")";
+                counter++;
+            
+                if (counter == 10) {
+                    clearInterval(cubeClick);
+                }
+            
+            }, 200);
+           
+            cubeClicked = false;
+
+            if (document.getElementById("middle").style.background == "url(" + "/slike/" + "6.png" + ")") {
+                var cubeClick = setInterval(function () {
+                    var randomNumber = cube[Math.floor(Math.random() * 6)];
+                    var images = "/slike/" + randomNumber + ".png";
+                    document.getElementById("middle").style.background = "url(" + images + ")" + "no-repeat";
+                    document.getElementById("middle").style.backgroundsize = "cover";
+                    counter++;
+    
+                    if (counter == 10) {
+                        clearInterval(cubeClick);
+                    }
+    
+                }, 200);
         }
+
+            cubeClicked = true;
+        }
+
     });
 }
 // Funkcija za biranje imena igraca
@@ -74,28 +98,28 @@ function changePlayer() {
 // Pomeranje figura, izvrsiti na click
 // Treba zavrsiti
 function moveFigure() {
-    
+
 }
 
 //Provera da li je na polju na koje treba da stanemo vec neko,
 // ukoliko jeste vratiti ga u njegovu bazu
 // Treba zavrsiti
 function returnToBase() {
-    
+
 }
 
 //Provera da li je i poslednja figura usla u kucicu
 // ukoliko jeste, objava pobede
 // Treba zavrsiti
 function victoryMove() {
-    
+
 }
 
 // Pisanje rezultata
 // ime igraca i rezultat cuvamo u local storage
 // Treba zavrsiti
 function finalResult() {
-    
+
 }
 
 // Pocetak igre
